10c86220ec7a154c1fd9199a39a3f79dc02ad761
Rafał Klimczak <rafal.klimczak.1989@gmail.com>
2021-07-04 11:41:51 +0200

    Update azurecred.py

--- azurecred.py
+++ azurecred.py
@@ -1,5 +1,5 @@
-AZDBSERVER = 'flaskapp.database.windows.net' #twoja nazwautworzonegoserwera
+AZDBSERVER = 'rafalklimczak.database.windows.net' #twoja nazwautworzonegoserwera
 AZDBNAME = 'flaskApp' #twojanazwa bazyna serwerze
-AZDBUSER = 'vassillizaitsev'#twoja nazwa użytkownika
-AZDBPW = 'Zwierciadlo12#'#twoje hasło,którego nie powinniśmy znać
+AZDBUSER = 'rafal'#twoja nazwa użytkownika
+AZDBPW = 'rafik1789!'#twoje hasło,którego nie powinniśmy znać
 AZDBDRIVER ='{ODBC Driver 17 for SQL Server}'#sterownikbazy danych, nic nie zmieniaj
